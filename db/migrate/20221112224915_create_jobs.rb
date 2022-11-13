f5c7688ef922ccca846ed4e56451a21ed316925a
Thermiee <navyblack16@yahoo.com>
2022-11-13 12:21:23 +0100

    create rails g scaffold Job

---
+++ db/migrate/20221112224915_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+      t.string :url
+      t.string :job_type
+      t.string :location
+      t.boolean :remote_ok
+      t.string :apply_url
+
+      t.timestamps
+    end
+  end
+end
